d5c24a22925ee04901b277b77e4b05146e6e7ed8
nzimermann <82411251+nzimermann@users.noreply.github.com>
2025-04-15 20:52:31 -0300

    CG_N2_1

---
+++ Unidade2/CG_N2_1/Circulo.cs
@@ -0,0 +1,53 @@
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK;
+using System;
+
+namespace gcgcg
+{
+    internal class Circulo : Objeto
+    {
+        public Circulo(Objeto _paiRef, ref char _rotulo) : this(_paiRef, ref _rotulo, 0.5f)
+        {
+
+        }
+
+        public Circulo(Objeto _paiRef, ref char _rotulo, float raio) : base(_paiRef, ref _rotulo)
+        {
+            base.PrimitivaTipo = PrimitiveType.Points;
+            PrimitivaTamanho = 4;
+
+            base.PontosAdicionar(new Ponto4D());
+
+            int numPoints = 70;
+
+            for (int i = 0; i < numPoints; i++)
+            {
+                float angulo = (i * 2 * MathF.PI) / numPoints;
+                float x = raio * MathF.Cos(angulo);
+                float y = raio * MathF.Sin(angulo);
+                base.PontosAdicionar(new Ponto4D(x, y));
+            }
+
+            Atualizar();
+        }
+
+        public void Atualizar()
+        {
+
+            base.ObjetoAtualizar();
+        }
+
+#if CG_Debug
+    public override string ToString()
+    {
+      System.Console.WriteLine("__________________________________ \n");
+      string retorno;
+      retorno = "__ Objeto Ponto _ Tipo: " + PrimitivaTipo + " _ Tamanho: " + PrimitivaTamanho + "\n";
+      retorno += base.ImprimeToString();
+      return retorno;
+    }
+#endif
+
+    }
+}
